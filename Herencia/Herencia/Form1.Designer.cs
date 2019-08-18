45123fda9a884106ce90182382e8130efd3441f7
Mirian Mendoza <Mirian Mendoza@MIRIAN>
2019-08-18 17:48:43 -0600

    Clases Herencia

---
+++ Herencia/Herencia/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Herencia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(127, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Procesar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(342, 175);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Herencia/Herencia/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Herencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var thor = new Heroe();
+            thor.Id = 01;
+            thor.Nombre = "Thor Dios del trueno";
+            thor.Poder = "Dios del trueno";
+
+            var capitanAmerica = new Heroe();
+            capitanAmerica.Id = 02;
+            capitanAmerica.Nombre = "Steven Rogers";
+            capitanAmerica.Poder = "Fuerza";
+
+            var hulk = new Heroe();
+            hulk.Id = 03;
+            hulk.Nombre = "BruceBaner";
+            hulk.Poder = "Fuerza";
+
+            var ultron = new Villano();
+            ultron.Id = 01;
+            ultron.Nombre = "Ultron";
+            ultron.Poder = "Hecho de vibranium";
+
+            var thanos = new Villano();
+            thanos.Id = 02;
+            thanos.Nombre = "El Thanos";
+            thanos.Poder = "Las gemas";
+
+            var loki = new Villano();
+            loki.Id = 03;
+            loki.Nombre = " Loki El Dios de la Mentira";
+            loki.Poder = "Arte del engaño";
+
+            var listaheroes = new List<Heroe>();
+            listaheroes.Add(capitanAmerica);
+            listaheroes.Add(hulk);
+            listaheroes.Add(thor);
+
+            foreach (var h in listaheroes)
+            {
+                MessageBox.Show(h.Nombre);
+            }
+        }
+    }
+}
